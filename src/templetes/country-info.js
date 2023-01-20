2f2e6b75636a5db283824a2625eb8f072ff5ca8e
figatix <60873339+figatix@users.noreply.github.com>
2023-01-20 22:12:01 +0200

    Update country-info.js

--- src/templetes/country-info.js
+++ src/templetes/country-info.js
@@ -1,13 +1,11 @@
 
-
 export function createCountryInfo(arrOfOneCountry) {
   const country = arrOfOneCountry[0];
-  console.log(country);
   const langs = Object.values(country.languages).join(', ')
 
   return `
   <img class="country-item__thumb-flag" src="${country.flags.svg}" alt="Flag of ${country.name.official}">
-      <span class="country-item__name" style = "font-size: large;">${country.name.common}</span>
+      <span class="country-item__name" style = "font-size: large;">${country.name.official}</span>
       <ul class="country-info__list">
         <li class="country-info__item">Capital: ${country.capital}</li>
         <li class="country-info__item">Population: ${country.population}</li>
