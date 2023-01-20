0b482dbd9be3503a7a5e442057fb55ddc13f9965
figatix <60873339+figatix@users.noreply.github.com>
2023-01-20 22:12:05 +0200

    Update country-list.js

--- src/templetes/country-list.js
+++ src/templetes/country-list.js
@@ -1,21 +1,13 @@
 
-// const templateList = `
-//   <li>
-//     <img src="" alt="">
-//     <p></p>
-//   </li>
-//   `;
-
 export function createCountryList(arrOfCountries) {
   const countryItemArr = arrOfCountries.map(el => {
     return `
       <li class = "country-item">
         <img class="country-item__thumb-flag" src="${el.flags.svg}" alt="Flag of ${el.name.official}">
-        <span class="country-item__name">${el.name.common}</span>
+        <span class="country-item__name">${el.name.official}</span>
       </li>
       `
   }).join('')
 
-  console.log(countryItemArr);
   return countryItemArr
 }
